761bc878b385c5e753aea3cbc2aaba58a7d7cdbf
aleph <aleph@riseup.net>
2014-12-15 20:31:30 +0100

    [frontend] So this is console command:  python manage.py startapp frontend

--- ataraxia_archive/ataraxia_archive/settings.py
+++ ataraxia_archive/ataraxia_archive/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'kernel'
+	'kernel',
+	'frontend'
 )
 
 MIDDLEWARE_CLASSES = (
