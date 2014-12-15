8d49b23cafe43606c4712a419ec665c73bb2b676
aleph <aleph@riseup.net>
2014-12-15 20:52:10 +0100

    [Home page] After this we can do: python manage.py 0.0.0.0:8080 and navigate through!

--- ataraxia_archive/ataraxia_archive/settings.py
+++ ataraxia_archive/ataraxia_archive/settings.py
@@ -79,7 +79,18 @@ USE_L10N = True
 USE_TZ = True
 
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "templates"),
+    os.path.join(BASE_DIR, "media"),
+)
+
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+MEDIA_BASE = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, MEDIA_BASE)
+MEDIA_URL = '/media/'
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
